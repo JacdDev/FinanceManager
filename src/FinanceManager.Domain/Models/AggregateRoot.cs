05349fd0b0ecbc2f71768d68c11d9151cb597631
JacdDev <joseantonio.caravacadiosdado@gmail.com>
2022-12-15 19:05:49 +0100

    Added Models base classes and fixed register localization

---
+++ src/FinanceManager.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Domain.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId> where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id) { 
+        }
+    }
+}
---
+++ src/FinanceManager.Domain/Models/ValueObject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Domain.Models
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetEquialityComponents();
+
+        public bool Equals(ValueObject? other)
+        {
+            return Equals((object?)other);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var valueObject = (ValueObject)obj;
+
+            return GetEquialityComponents()
+                .SequenceEqual(valueObject.GetEquialityComponents());
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEquialityComponents()
+                .Select(x=>x?.GetHashCode() ?? 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+    }
+}
